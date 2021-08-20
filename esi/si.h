c4ea632d3550329a1e7b858faa911bba967215f6
johnthepepper <kinetixcicocuk@gmail.com>
2021-08-20 17:17:50 +0300

    ESI register support

---
+++ esi/si.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace reg_c{
+    extern int si_reg(char* a);
+}
+
+namespace reg_i{
+    extern int si_reg(int* a);
+}
+
+namespace reg_f{
+    extern int si_reg(float* a);
+}
+
+namespace reg_d{
+    extern int si_reg(double* a);
+}
