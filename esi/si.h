99f6d7cc4c53e4e993c4cdacf3e64a2d06060ed0
johnthepepper <kinetixcicocuk@gmail.com>
2021-08-21 22:42:25 +0300

    please check my codes

--- esi/si.h
+++ esi/si.h
@@ -1,17 +1,3 @@
 #pragma once
 
-namespace reg_c{
-    extern int si_reg(char* a);
-}
-
-namespace reg_i{
-    extern int si_reg(int* a);
-}
-
-namespace reg_f{
-    extern int si_reg(float* a);
-}
-
-namespace reg_d{
-    extern int si_reg(double* a);
-}
+extern int si_reg(void* a);
